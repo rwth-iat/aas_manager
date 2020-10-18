833e4afa69f72b16717c84344bf1f7682262ce68
zrgt <garmaev@gmx.de>
2020-10-18 16:24:01 +0200

    Add test aasx import for future tests

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from aas_editor.editorApp import EditorApp
 from PyQt5 import QtWidgets
 from aas.examples.data.example_aas import create_full_example
 
-
 def main():
     app = QtWidgets.QApplication(sys.argv)
 
     obj_store = create_full_example()
+    # obj_store = DictObjectStore()
+    # file_store = DictSupplementaryFileContainer()
+    # reader = aasx.AASXReader("/Sample_AAS/07_PhoenixContact.aasx")
+    # reader.read_into(obj_store, file_store)
 
     window = EditorApp()
     window.importTestPack(obj_store)
