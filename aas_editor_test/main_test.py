d3642e5de97961dfd90b607c3eaa308717979fa3
garmaev <igor.garmaev@fit.fraunhofer.de>
2020-10-24 19:56:45 +0200

    Update main test

--- aas_editor_test/main_test.py
+++ aas_editor_test/main_test.py
@@ -16,6 +16,7 @@ from aas_editor.views.treeview_pack import PackTreeView
 
 app = QApplication(sys.argv)
 
+
 class TestUtilFuncs(TestCase):
     def test_main(self):
         def test1():
@@ -36,6 +37,24 @@ class TestUtilFuncs(TestCase):
                 #     sleep(0.5)
                 # print(pack_index.data(NAME_ROLE))
 
+        def test2():
+            try:
+                tab1: Tab = window.tabWidget.widget(0)
+                attrsTreeView = tab1.attrsTreeView
+                attrsTreeView.expandAll()
+                try:
+                    attrsGenerator = attrGenList[0]
+                    attr_index = next(attrsGenerator)
+                    attrsTreeView.setCurrentIndex(attr_index)
+                    print(attr_index.data(NAME_ROLE))
+                except (StopIteration, TypeError):
+                    pack_index = next(itemsGenerator)
+                    packTreeView.setCurrentIndex(pack_index)
+                    attrGenList[0] = attrsTreeView.model().iterItems()
+            except StopIteration:
+                print("Test is completed")
+
+
         obj_store = create_full_example()
 
         window = EditorApp()
@@ -45,10 +64,12 @@ class TestUtilFuncs(TestCase):
 
         packTreeView: PackTreeView = window.packItemsTreeView
         packTreeView.expandAll()
+        itemsGenerator = packTreeView.model().iterItems()
+        attrGenList = [None]
 
         # QTimer.singleShot(1000, test1)
         timer = QTimer()
         timer.setInterval(500)
-        timer.timeout.connect(test1)
+        timer.timeout.connect(test2)
         timer.start()
         app.exec_()
