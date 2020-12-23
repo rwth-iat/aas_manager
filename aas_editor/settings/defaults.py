91301481ecc29bf351d21ff51bffb425dad495ae
zrgt <garmaev@gmx.de>
2020-12-23 20:08:16 +0100

    Add mime_type default completions

--- aas_editor/settings/defaults.py
+++ aas_editor/settings/defaults.py
@@ -1,4 +1,37 @@
-from aas.model import Key, KeyType, Asset, AssetKind, Identifier, IdentifierType
+from aas.model import Key, KeyType, Asset, AssetKind, Identifier, IdentifierType, File, Blob
+
+MIME_TYPES = [
+    "application/graphql",
+    "application/javascript",
+    "application/json",
+    "application/ld+json",
+    "application/msw",
+    "application/pdf",
+    "application/sql",
+    "application/vnd.api+json",
+    "application/vnd.ms-excel",
+    "application/vnd.ms-powerpoint",
+    "application/vnd.oasis.opendocument.text",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/x-www-form-urlencoded",
+    "application/xml",
+    "application/zip",
+    "application/zstd",
+    "audio/mpeg",
+    "audio/ogg",
+    "image/gif",
+    "image/jpeg",
+    "image/png",
+    "multipart/form-data",
+    "text/css",
+    "text/csv",
+    "text/html",
+    "text/php",
+    "text/plain",
+    "text/xml",
+]
 
 DEFAULTS = {
     Key: {
@@ -17,5 +50,11 @@ DEFAULTS = {
 DEFAULT_COMPLETIONS = {
     Key: {
         "value": []
-    }
+    },
+    File: {
+        "mime_type": MIME_TYPES
+    },
+    Blob: {
+        "mime_type": MIME_TYPES
+    },
 }
