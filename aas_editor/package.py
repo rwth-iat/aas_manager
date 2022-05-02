9a203503105ac6aa30e429cd7f7a6dde3cc0b5f9
zrgt <garmaev@gmx.de>
2022-05-02 16:17:58 +0200

    Fix issue with CDs

--- aas_editor/package.py
+++ aas_editor/package.py
@@ -130,7 +130,7 @@ class Package:
         """Add references of all existing CD to concept dictionary in existing AAS."""
         #TODO: fix if pyi40aas changes
         for shell in self.shells:
-            dictionary = concept.ConceptDictionary(id_short="ConceptDictionary")
+            dictionary = shell.concept_dictionary if shell.concept_dictionary else concept.ConceptDictionary("CD")
             for cd in self.concept_descriptions:
                 reference = AASReference.from_referable(cd)
                 dictionary.concept_description.add(reference)
