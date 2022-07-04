4d848128a7b7ef35e384b564cf7c6de0c7ee6b61
zrgt <garmaev@gmx.de>
2022-07-04 15:37:51 +0200

    Add mapping and import utils

--- aas_editor/import_feature/import_util.py
+++ aas_editor/import_feature/import_util.py
@@ -23,6 +23,7 @@ import basyx
 import openpyxl
 from basyx.aas import model
 from basyx.aas.model import AASReference, Key
+from openpyxl.worksheet.worksheet import Worksheet
 
 from aas_editor.import_feature.import_settings import MAPPING_ATTR
 from aas_editor.package import Package
@@ -31,30 +32,78 @@ from aas_editor.utils.util_type import isIterable
 COLUMNS_PATTERN = re.compile(r"\$[A-Z][A-Z]?\$")
 
 
-def _saveMapping4referable(obj, mapDict):
+def colLettersInExcelSheet(sheet: Worksheet) -> List[str]:
+    colLetters = []
+    for columnNum in range(1, sheet.max_column + 1):
+        colLetters.append(openpyxl.utils.cell.get_column_letter(columnNum))
+    return colLetters
+
+
+def importRowValueFromExcel(sourcefile, sheetname=None, row=2):
+    excel_file = openpyxl.load_workbook(sourcefile, data_only=True)
+    if not sheetname:
+        sheetname = excel_file.sheetnames[0]
+    sheet = excel_file[sheetname]
+
+    rowValue: Dict = {}
+    for colLetter in colLettersInExcelSheet(sheet):
+        rowValue[colLetter] = str(sheet[f"{colLetter}{row}"].value)  # TODO Fixme
+    return rowValue
+
+
+def _mapping4referableIntoDict(obj, mapDict):
     mapping = getattr(obj, MAPPING_ATTR, {})
     if mapping:
         ref = AASReference.from_referable(obj)
-        keys = ','.join([f"Key(type_={i.type}, local={i.local}, id_type={i.id_type}, value='{i.value}')" for i in ref.key])
+        keys = ','.join(
+            [f"Key(type_={i.type}, local={i.local}, id_type={i.id_type}, value='{i.value}')" for i in ref.key])
         target_type = repr(ref.type).lstrip("<class '").rstrip("'>")
         mapDict[f"AASReference(target_type={target_type}, key=({keys},))"] = mapping
 
 
-def _saveMapping(obj, mapDict):
-    _saveMapping4referable(obj, mapDict)
+def _mappingIntoDict(obj, mapDict):
+    """Save all existing mappings in the obj into mapDict"""
+    _mapping4referableIntoDict(obj, mapDict)
     if isIterable(obj):
         for i in obj:
-            _saveMapping(i, mapDict)
+            _mappingIntoDict(i, mapDict)
+
+
+def getMapping(pack: Package):
+    mapDict = dict()
+    for obj in pack.objStore:
+        _mappingIntoDict(obj, mapDict)
+    return mapDict
 
 
 def saveMapping(pack: Package, file: str) -> bool:
-    mapDict = dict()
-    for obj in pack.objStore:
-        _saveMapping(obj, mapDict)
+    mapDict = getMapping(pack)
     with open(file, 'w') as jsonFile:
         json.dump(mapDict, jsonFile)
         jsonFile.close()
     return True
+
+
+def usedColumnsInMapping(mapDict) -> List[str]:
+    mapping = str(mapDict)
+    columns: List[str] = re.findall(COLUMNS_PATTERN, mapping)
+    columns.sort()
+    columns = list(set([col.strip("$") for col in columns]))
+    return columns
+
+
+def unusedColumnsInMapping(mapDict, sourcefile, sheetname: Worksheet) -> List[str]:
+    excel_file = openpyxl.load_workbook(sourcefile, data_only=True)
+    if not sheetname:
+        sheetname = excel_file.sheetnames[0]
+    sheet = excel_file[sheetname]
+
+    usedColumns = usedColumnsInMapping(mapDict)
+    columns = colLettersInExcelSheet(sheet)
+    for col in usedColumns:
+        columns.remove(col)
+    columns.sort()
+    return columns
 
 
 def setMappingFromFile(pack: Package, mappingFile: str):
@@ -74,20 +123,23 @@ def setMappingFromFile(pack: Package, mappingFile: str):
         setattr(refObj, MAPPING_ATTR, mapping)
 
 
-def importValue(value, sourcefile, row=2):
-    excel_file = openpyxl.load_workbook(sourcefile, data_only=True)
-    sheetname = excel_file.sheetnames[0]
-    sheet = excel_file[sheetname]
+def importValueFromExcelWB(value, workbook: openpyxl.Workbook, sheetname, row=2):
+    sheet = workbook[sheetname]
 
     columns: List[str] = re.findall(COLUMNS_PATTERN, value)
     for col in columns:
         column = col.strip("$")
-        importedVal = sheet[f"{column}{row}"].value
+        importedVal = str(sheet[f"{column}{row}"].value)  # FIXME
         value = value.replace(f"${column}$", importedVal, -1)
     return value
 
 
-def importValueFromExampleRow(rawValue: str, row:Dict):
+def importValueFromExcel(value, sourcefile, sheetname, row=2):
+    excel_file = openpyxl.load_workbook(sourcefile, data_only=True)
+    return importValueFromExcelWB(value, excel_file, sheetname, row)
+
+
+def importValueFromExampleRow(rawValue: str, row: Dict):
     value = rawValue
     columns: List[str] = re.findall(COLUMNS_PATTERN, rawValue)
     for col in columns:
@@ -101,15 +153,3 @@ def isValueToImport(value):
     if re.search(COLUMNS_PATTERN, value):
         return True
     return False
-
-
-def importRowValue(sourcefile, row=2):
-    excel_file = openpyxl.load_workbook(sourcefile, data_only=True)
-    sheetname = excel_file.sheetnames[0]
-    sheet = excel_file[sheetname]
-
-    rowValue: Dict = {}
-    for columnNum in range(1,sheet.max_column+1):
-        column = openpyxl.utils.cell.get_column_letter(columnNum)
-        rowValue[column] = sheet[f"{column}{row}"].value
-    return rowValue
