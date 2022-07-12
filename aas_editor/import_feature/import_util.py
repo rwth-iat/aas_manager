6cefd03f2ebe31110126ec501c6973d2795c9ddc
zrgt <garmaev@gmx.de>
2022-07-13 01:43:00 +0200

    Sort usedColumns

--- aas_editor/import_feature/import_util.py
+++ aas_editor/import_feature/import_util.py
@@ -87,8 +87,9 @@ def saveMapping(pack: Package, file: str) -> bool:
 def usedColumnsInMapping(mapDict) -> List[str]:
     mapping = str(mapDict)
     columns: List[str] = re.findall(COLUMNS_PATTERN, mapping)
-    columns.sort(key=len)
     columns = list(set([col.strip("$") for col in columns]))
+    columns.sort()
+    columns.sort(key=len)
     return columns
 
 
