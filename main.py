afb07593164182b13579f7d6951904be011f1bbb
zrgt <garmaev@gmx.de>
2022-08-31 03:26:16 +0200

    Small bug fix

--- main.py
+++ main.py
@@ -9,11 +9,14 @@
 #  A copy of the GNU General Public License is available at http://www.gnu.org/licenses/
 
 import sys
+
+from PyQt5.QtCore import Qt
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5 import QtWidgets
 
 
 def main():
+    QtWidgets.QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     app = QtWidgets.QApplication(sys.argv)
     from aas_editor.editorApp import EditorApp as CurrentApp
     from aas_editor.splash import Splash
