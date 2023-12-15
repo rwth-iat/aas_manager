65c7bab20b408e45942ebae3906a15a401b4330b
zrgt <garmaev@gmx.de>
2023-12-15 18:26:22 +0100

    Remove addable groupbox type
    It is not used anymore
    
    (cherry picked from commit dcfc74fdf29350d3f2b842d47b023f8a6fbf22ff)

--- aas_editor/dialogs.py
+++ aas_editor/dialogs.py
@@ -304,7 +304,6 @@ class AddObjDialog(AddDialog):
 class GroupBoxType(Enum):
     SIMPLE = 0
     CLOSABLE = 1
-    ADDABLE = 2
 
 
 class GroupBox(QGroupBox):
@@ -333,13 +332,6 @@ class GroupBox(QGroupBox):
 
     def isClosable(self) -> bool:
         return self.isCheckable() and self.type is GroupBoxType.CLOSABLE
-
-    def setAddable(self, b: bool) -> None:
-        self.type = GroupBoxType.ADDABLE if b else GroupBoxType.SIMPLE
-        self.setCheckable(b)
-
-    def isAddable(self) -> bool:
-        return self.isCheckable() and self.type is GroupBoxType.ADDABLE
 
     def setVal(self, val):
         pass
