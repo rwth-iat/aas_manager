2cdad5b2eda309ac0f1dead8a29fe1a4cb19b690
monsieuremre <e.paker@iat.rwth-aachen.de>
2024-06-30 14:58:10 +0000

    pytest implement

--- test.py
+++ test.py
@@ -10,68 +10,99 @@
 
 import sys
 import logging
-import time
-import signal
-from PyQt6.QtCore import Qt, pyqtSignal, QModelIndex, QTimer, QAbstractItemModel, QPoint
-from PyQt6.QtGui import QClipboard, QPalette, QColor, QMouseEvent, QKeyEvent, QAction
+
+import pytest
+
 from PyQt6 import QtWidgets
-from aas_editor import *
+from PyQt6 import QtWebEngineWidgets
+from PyQt6.QtWidgets import *
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QAbstractButton
+from PyQt6.QtCore import Qt
+from PyQt6.QtTest import QTest
 
 logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w",
                     format="%(asctime)s | %(levelname)s | %(message)s")
 
+@pytest.fixture
+def app(qtbot):
+    app = QtWidgets.QApplication("./main.py")
+    return app
+        
+def find_button_by_text(widget, text):
+    if isinstance(widget, QToolButton) and widget.text() == text:
+        return widget
 
-# class TimeOutException(Exception):
-#    pass
-#
-#
-# def alarm_handler(signum, frame):
-#    raise TimeOutException()
+    for child in widget.findChildren(QWidget):
+        result = find_button_by_text(child, text)
+        if result:
+            return result
 
+    return None
+    
+def find_menu_by_text(widget, text):
+    if isinstance(widget, QMenu) and widget.title() == text:
+        return widget
 
-logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w",
-                    format="%(asctime)s | %(levelname)s | %(message)s")
+    for child in widget.findChildren(QWidget):
+        result = find_menu_by_text(child, text)
+        if result:
+            return result
 
+    return None
+    
+def print_widgets_text(widget, depth=0):
+    if isinstance(widget, QMenu):
+        print("  " * depth + widget.title())
+    elif isinstance(widget, QToolButton):
+        print("  " * depth + widget.text())
+    else:
+        print("  " * depth + widget.objectName() + " - " + widget.metaObject().className())
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
+    for child in widget.findChildren(QWidget):
+        print_widgets_text(child, depth + 1)
 
+def click_widget(widget):
+    if widget:
+        QTest.mouseClick(widget, Qt.LeftButton)
+        
+def test_gui(qtbot):
     from aas_editor.editorApp import EditorApp as CurrentApp
-    from basyx.aas.model import Submodel
-
-    fileToOpen = sys.argv[1] if len(sys.argv) > 1 else None
-    window = CurrentApp(fileToOpen)
+    window = CurrentApp("./test.aasx")
     window.show()
-
+    qtbot.addWidget(window)
+    qtbot.waitUntil(window.isVisible)
+    assert window.isVisible(), f"Window not visible"
+    
     tree = window.mainTreeView
     tree.expandAll()
     model = tree.model()
-    root = model.index(0, 0)
 
     if root.isValid():
-        root_data = model.data(root)
-        print(f"Iterating Through {root_data}")
-        for row in range(model.rowCount(root)):
-            item_index = model.index(row, 0, root)
-            item_data = model.data(item_index)
-            print(f"Itering Items Under {item_data}")
-            for subrow in range(model.rowCount(item_index)):
-                subitem_index = model.index(subrow, 0, item_index)
-                subitem_data = model.data(subitem_index)
-                print(f"Item: {subitem_data}")
-
-            # if item_data == "submodels":
-            #    signal.signal(signal.SIGALRM, alarm_handler)
-            #    signal.alarm(1)
-            #    try:
-            #        print("Add dialog for a new submodel")
-            #        tree.addItemWithDialog(item_data, Submodel)
-            #    except:
-            #        print("Dialog timed out")
-            #    signal.alarm(0)
-    app.exec()
+    root_data = model.data(root)
+    print(f"Iterating Through {root_data}")
+    for row in range(model.rowCount(root)):
+        item_index = model.index(row, 0, root)
+        item_data = model.data(item_index)
+        print(f"Itering Items Under {item_data}")
+        for subrow in range(model.rowCount(item_index)):
+            subitem_index = model.index(subrow, 0, item_index)
+            subitem_data = model.data(subitem_index)
+            print(f"Item: {subitem_data}")
 
 
-if __name__ == "__main__":
 
-    main()
+    subm = find_menu_by_text(window, "Add existing submodel")
+    click_widget(subm)
+    
+    QTest.keyPress(window, Qt.Key_N, Qt.ControlModifier)
+    QTest.keyRelease(window, Qt.Key_N, Qt.ControlModifier)
+    
+    current_focus_window = None
+    
+    for widget in QApplication.topLevelWidgets():
+        #print(widget)
+        if widget.isActiveWindow():
+            current_focus_window = widget
+            break
+            
+    #print(current_focus_window)
